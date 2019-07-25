13a9149144c247b2bd60b285194e2176bc340e8f
llabake <lemboyelabake@gmail.com>
2019-07-25 20:14:16 +0100

    Add CSS to posts on the home page - add CSS to the posts.scss file - add CSS to the mobile.scss to fix too large text issues on smaller screens

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
 //= require activestorage
 //= require turbolinks
 //= require_tree .
 
-//= require jquery
 //= require bootstrap-sprockets
