cadaf1ebe41be45e73deaa932c117ed3d8612ea2
llabake <lemboyelabake@gmail.com>
2019-07-26 12:30:35 +0100

    Add specs for the post_format_partial_path helper method

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -6,25 +6,24 @@ RSpec.describe PostsHelper, type: :helper do
   context '#create_new_post_partial_path' do
     it "returns a signed_in partial's path" do
       helper.stub(:user_signed_in?).and_return(true)
-      expect(helper.create_new_post_partial_path). to (eq 'posts/branch/create_new_post/signed_in'
+      expect(helper.create_new_post_partial_path). to (eq 'posts/branch/create_new_post/signed_in')
     end
 
     it "returns a signed_in partial's path" do
       helper.stub(:user_signed_in?).and_return(false)
-      expect(helper.create_new_post_partial_path). to (eq 'posts/branch/create_new_post/not_signed_in' )
+      expect(helper.create_new_post_partial_path). to (eq 'posts/branch/create_new_post/not_signed_in')
     end
   end
 
   context '#all_categories_button_partial_path' do
     it "returns an all_selected partial's path" do
       controller.params[:category] = ''
-      expect(helper.all_categories_button_partial_path).to(eq 'posts/branch/categories/all_selected'
-      )
+      expect(helper.all_categories_button_partial_path).to(eq 'posts/branch/categories/all_selected')
     end
 
     it "returns an all_not_selected partial's path" do
       controller.params[:category] = 'category'
-      expect(helper.all_categories_button_partial_path).to(eq 'posts/branch/categories/all_not_selected' )
+      expect(helper.all_categories_button_partial_path).to(eq 'posts/branch/categories/all_not_selected')
     end
   end
 
@@ -37,6 +36,18 @@ RSpec.describe PostsHelper, type: :helper do
     it "returns an empty partial's path" do
       assign(:posts, [1])
       expect(helper.no_posts_partial_path).to(eq 'shared/empty_partial')
+    end
+  end
+
+  context '#post_format_partial_path' do
+    it "returns a home_page partial's path" do
+      helper.stub(:current_page?).and_return(true)
+      expect(helper.post_format_partial_path).to('posts/post/home_page')
+    end
+
+    it "returns a branch_page partial's path" do
+      helper.stub(:current_page?).and_return(false)
+      expect(helper.post_format_partial_path).to(eq 'posts/post/branch_page')
     end
   end
 end
