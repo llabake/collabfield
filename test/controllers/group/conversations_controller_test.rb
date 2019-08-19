9090d1115cf5b19a83338f0b3c1099026c41b223
llabake <lemboyelabake@gmail.com>
2019-08-19 18:04:57 +0100

    Create back-end for creating a new group conversation - Create a Group::ConversationsController   Define a create action and add_to_conversations,   create_group_conversation and already_added? private methods inside - Create a  Group::NewConversationService and write specs for it

---
+++ test/controllers/group/conversations_controller_test.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class Group::ConversationsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
