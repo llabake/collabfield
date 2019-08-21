ecd0799c5a5555ebce5a75b41755375f53955d81
llabake <lemboyelabake@gmail.com>
2019-08-21 18:14:09 +0100

    Add a UI on private conversation to create a group conversations

--- app/helpers/private/conversations_helper.rb
+++ app/helpers/private/conversations_helper.rb
@@ -91,6 +91,20 @@ module Private::ConversationsHelper
     end
   end
 
+  def contacts_except_recipient(recipient)
+    contacts = current_user.all_active_contacts
+    # return all contacts, except the opposite user of the chat
+    contacts.delete_if { |contact| contact.id == recipient.id }
+  end
+
+  def create_group_conv_partial_path(_contact)
+    if recipient_is_contact?
+      'private/conversations/conversation/heading/create_group_conversation'
+    else
+      'shared/empty_partial'
+    end
+  end
+
   private
 
   def request_sent_by_user(contact)
