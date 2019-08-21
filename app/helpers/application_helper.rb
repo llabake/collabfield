c9b756779f00897c7fc4b2d17586529181b9f464
llabake <lemboyelabake@gmail.com>
2019-08-21 18:59:46 +0100

    - Create a _conversation.html.erb inside the group/conversations - Define a add_people_to_group_conv_list and write specs for it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,5 @@ module ApplicationHelper
   include PostsHelper
   include Private::ConversationsHelper
   include Private::MessagesHelper
+  include Group::ConversationsHelper
 end
--- app/helpers/group/conversations_helper.rb
+++ app/helpers/group/conversations_helper.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module Group::ConversationsHelper
+  def add_people_to_group_conv_list(conversation)
+    contacts = current_user.all_active_contacts
+    users_in_conv = conversation.users
+    add_people_to_conv_list = []
+    contacts.each do |contact|
+      # if the contact is already in the conversation, remove it from the list
+      add_people_to_conv_list << contact unless users_in_conv.include?(contact)
+    end
+    add_people_to_conv_list
+  end
 end
