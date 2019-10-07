071b924eacb84b6b165b0edae8dc5f428c186354
llabake <lemboyelabake@gmail.com>
2019-10-07 14:34:14 +0100

    Create a template for the MessengersController's index action

--- app/helpers/messengers_helper.rb
+++ app/helpers/messengers_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module MessengersHelper
+  def conversations_list_item_partial_path(conversation)
+    # if it's a private conversation
+    if conversation.class == Private::Conversation
+      'messengers/index/conversations_list_item/private'
+    else # it is a group conversation
+      'messengers/index/conversations_list_item/group'
+    end
+  end
 end
